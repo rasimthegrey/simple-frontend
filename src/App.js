3f157b6c7d471c43c4e92ed2b3e55f27135d64c2
Rasim <rasim.mutlu99@gmail.com>
2023-09-08 17:25:24 +0300

    changed paragraph test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Docker is awesome and excellent!
+          This is a CI test!
         </p>
         <a
           className="App-link"
