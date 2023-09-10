c2400ab6fa5723cdae4c6b77f36b7eb0ef124191
Rasim <rasim.mutlu99@gmail.com>
2023-09-10 14:08:21 +0300

    new test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CI/CD', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Continuous Integration and Continuous Deployment/i);
   expect(linkElement).toBeInTheDocument();
 });
 
