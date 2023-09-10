6d9bef9e1d9decb74b59b46fa92465b3679f176e
Rasim <rasim.mutlu99@gmail.com>
2023-09-10 14:07:12 +0300

    changed text

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           This is a CI test!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Continuous Integration and Continuous Deployment</p>
       </header>
     </div>
   );
